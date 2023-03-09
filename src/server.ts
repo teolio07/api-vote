a836742c03eed8c360cd028b789294d7e98e6ca2
Jheison Rua <ruateo555@gmail.com>
2023-03-09 15:50:46 -0500

    add cors

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import { apiRouter } from './routes';
-
+import cors from 'cors';
 //----Conection
 import './DB/connect'
 
@@ -18,6 +18,21 @@ const port = process.env.PORT || 4002
 
 //uses
 app.use(express.json());
+
+
+//cors
+var whitelist = ['http://example1.com', 'http://example2.com','http://localhost']
+var corsOptions = {
+  origin: function (origin:any, callback:any) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors())
+
 
 
 //------router
